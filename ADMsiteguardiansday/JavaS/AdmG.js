35c72e87fafa72f93c9dd33726cc147ee9788728
Dayoliver16 <dayaneoliveiraresendetiago@gmail.com>
2026-06-18 11:13:30 -0300

    Add files via upload

---
+++ ADMsiteguardiansday/JavaS/AdmG.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    $('.button-collapse').sideNav();
+    $('.collapsible').collapsible();
+    $('.dropdown-button').dropdown({
+        hover: false,
+        belowOrigin: true
+    });
+});
