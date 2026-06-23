ffbb9d111f4a868b2f84a4e57c6db75974ebb951
Dayoliver16 <dayaneoliveiraresendetiago@gmail.com>
2026-06-23 03:01:59 -0300

    Delete ADMsiteguardiansday directory

--- ADMsiteguardiansday/JavaS/AdmG.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function(){
-    $('.button-collapse').sideNav();
-    $('.collapsible').collapsible();
-    $('.dropdown-button').dropdown({
-        hover: false,
-        belowOrigin: true
-    });
-});
