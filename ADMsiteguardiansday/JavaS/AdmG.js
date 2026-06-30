a0b18427570291c995c8b980a631ed5cc649f172
Dayoliver16 <dayaneoliveiraresendetiago@gmail.com>
2026-06-30 06:16:37 -0300

    fazendo as PAD da seção adm

--- ADMsiteguardiansday/JavaS/AdmG.js
+++ ADMsiteguardiansday/JavaS/AdmG.js
@@ -1,9 +1,5 @@
-
 $(document).ready(function(){
-    $('.button-collapse').sideNav();
-    $('.collapsible').collapsible();
-    $('.dropdown-button').dropdown({
-        hover: false,
-        belowOrigin: true
-    });
+        $('.button-collapse').sideNav();
+        $('.collapsible').collapsible();
+        $('.fixed-action-btn').floatingActionButton();
 });
